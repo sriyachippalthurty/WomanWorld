f8594dfe4a24950888aefd49f1d20b00045a4229
Vedha Sirur <vedhamsirur@gmail.com>
2022-06-29 13:35:37 -0500

    added show segues and launch screens

---
+++ WomanWorld/AutoChangeScript.swift
@@ -0,0 +1,21 @@
+//
+//  AutoChangeScript.swift
+//  WomanWorld
+//
+//  Created by Scholar on 6/29/22.
+//
+
+import UIKit
+
+class AutoChangeScript: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            
+            self.performSegue(withIdentifier: "autoChangeScreens", sender: self)
+        }
+    }
+    
+
+}
