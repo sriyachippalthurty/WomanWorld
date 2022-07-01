088833bc79537095922f5f2f1512356561683603
vedha <vedhamsirur@gmail.com>
2022-06-30 21:44:58 -0500

    education text

---
+++ WomanWorld/EducationTextViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EducationTextViewController.swift
+//  WomanWorld
+//
+//  Created by Vedha Sirur on 6/30/22.
+//
+
+import UIKit
+
+class EducationTextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func un(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
